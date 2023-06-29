5ae8d1055a7f0fe4bd2fa674654a06934bf72acc
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-29 11:32:24 +0200

    Update forms.py

--- Auftraege/forms.py
+++ Auftraege/forms.py
@@ -44,7 +44,7 @@ class Position(ModelForm):
       'anzahl': forms.NumberInput(attrs={'class':'form-control'}),
       'unterschrift_datum':forms.TextInput(attrs={'class':'form-control mx-auto','readonly':'readonly'}),
       'unterschrift':forms.Textarea(attrs={'class':'form-control mx-auto','rows':'1','readonly':'readonly'}),
-      'fahrzeug':forms.TextInput(choices=VEHICLE_CHOICES, attrs={'class':'form-control mx-auto'}),
+      'fahrzeug':forms.TextInput(attrs={'class':'form-control mx-auto'}),
       'wartezeit':forms.NumberInput(attrs={'class':'form-control'}),
       'pauschale':forms.NumberInput(attrs={'class':'form-control'})
     }  
