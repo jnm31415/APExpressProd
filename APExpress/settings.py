6c04f645ec2e11a3df393617c16b436b9da6ddd5
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-07-04 07:38:48 +0200

    Update settings.py

--- APExpress/settings.py
+++ APExpress/settings.py
@@ -31,6 +31,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'apexpresslogistik@gmail.com'
 EMAIL_HOST_PASSWORT = 'ZCC12345'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 ALLOWED_HOSTS = ['*','.vercel.app']
 
