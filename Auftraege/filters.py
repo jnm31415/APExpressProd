c5d24edfb42eef1e2a6a5ff18d41c3dfe7ec34ae
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-07-25 11:16:02 +0200

    Update filters.py

--- Auftraege/filters.py
+++ Auftraege/filters.py
@@ -10,7 +10,7 @@ class SearchPosition(django_filters.FilterSet):
     von = ModelChoiceFilter(queryset=Adressen.objects.all(),field_name="von", widget=forms.Select(attrs={'class':'form-select form-select-sm mx-auto'}))
     nach = ModelChoiceFilter(queryset=Adressen.objects.all(),field_name="nach", widget=forms.Select(attrs={'class':'form-select form-select-sm mx-auto'}))
     kostenstelle = CharFilter(field_name="kostenstelle", widget=forms.TextInput(attrs={'class':'form-control form-control-sm'}))
-    referenz = CharFilter(field_name="referenz", widget=forms.TextInput(attrs={'class':'form-control form-control-sm'}))
+    referenz = CharFilter(field_name="referenz", lookup_expr='icontains', widget=forms.TextInput(attrs={'class':'form-control form-control-sm'}))
     empfänger = CharFilter(field_name="empfänger", widget=forms.TextInput(attrs={'class':'form-control form-control-sm'}))
     unterschrift_datum = DateFilter(field_name='unterschrift_datum',widget=forms.TextInput(attrs={'class':'form-control form-control-sm','type':'date'}))
     "start_date = DateFilter(field_name='unterschrift_datum',lookup_expr=('lt'),widget=forms.TextInput(attrs={'class':'form-control form-control-sm','type':'date'}))"
