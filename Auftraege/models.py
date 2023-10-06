36f00db744ecfe9cc60b30d9974ace4fcea4d146
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-10-06 22:33:24 +0200

    Update models.py

--- Auftraege/models.py
+++ Auftraege/models.py
@@ -79,7 +79,7 @@ class Auftragspositionen(models.Model):
     von = models.ForeignKey(Adressen,null=True,blank=True,on_delete=models.PROTECT, related_name='von')
     nach = models.ForeignKey(Adressen, null=True,blank=True,on_delete=models.PROTECT, related_name='nach')
     kostenstelle = models.CharField(max_length=20,blank=True,null=True)
-    referenz = models.TextField(blank=True,null=True)
+    referenz = models.TextField(blank=False,null=True)
     einzelpreis = models.FloatField(blank=True, null=True,default=0)
     mwst = models.IntegerField(blank=True,null=True,default=19)
     anzahl = models.FloatField(blank=True, null=True, default=1) 
